e8ec09b9deeaea239b6a529031e094664c17da2b
Samson Okaludo <samson@divercity.io>
2022-01-10 10:57:31 +0100

    project setup

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {});
+
+const studentsRouter = require("./routes/students");
+const coursesRouter = require("./routes/courses");
+
+
+app.use("/students", studentsRouter);
+app.use("/courses", coursesRouter);
+
+
+module.exports = app;
